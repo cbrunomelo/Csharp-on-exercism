287d06c9b984b6f919ce62cb2387a1d09d1ac819
Bruno <c.brunomelo@outlook.com>
2022-08-02 13:31:53 -0300

    add circular_buffer

---
+++ circulpar_buffer.cs
@@ -0,0 +1,68 @@
+public class Circular_buffer
+{
+    public int[] Buffer;
+
+    public Circular_buffer(int size)
+    {
+        Buffer = new int[size];
+        Buffer[3] = 1;
+    }
+
+
+    public void add()
+    {
+        int Indice = Array.IndexOf(Buffer, Buffer.Max());
+        if (Buffer[NextIndice(Indice)] == 0)
+        {
+            Buffer[NextIndice(Indice)] = Buffer.Max() + 1;
+
+        }
+        else
+        {
+            Console.WriteLine("Overwrite ? 1 - Sim, 2 - Não");
+            int answer = Convert.ToInt32(Console.ReadLine());
+            if (answer == 1)
+                Overwrite();
+            Show();
+        }
+    }
+
+    public void Show()
+    {
+        foreach (var numbers in Buffer)
+        {
+            Console.WriteLine(numbers);
+        }
+    }
+
+
+    public int NextIndice(int Indice)
+    {
+        Indice += 1;
+        while (Indice >= Buffer.Length)
+        {
+            Indice = Indice - Buffer.Length;
+        }
+        return Indice;
+    }
+
+    public void Overwrite()
+    {
+        int Indice = Array.IndexOf(Buffer, Buffer.Min());
+        Buffer[Indice] = Buffer.Max() + 1;
+        Buffer[NextIndice(Indice)] = Buffer.Max() + 2;
+    }
+
+    public void Remove()
+    {
+        int Indice = Array.IndexOf(Buffer, Buffer.Min());
+        while (Buffer[Indice] == 0)
+        {
+            Indice = NextIndice(Indice);
+        }
+
+        Buffer[Indice] = 0;
+        Buffer[NextIndice(Indice)] = 0;
+
+    }
+}
