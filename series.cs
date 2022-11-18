c827be830dee5a02bd24fe58d48b184edb5affe9
Bruno <c.brunomelo@outlook.com>
2022-11-18 09:44:18 -0300

    add series

---
+++ series.cs
@@ -0,0 +1,11 @@
+public static class Series
+{
+    public static IEnumerable<string> Slices(string input, int length)
+    {
+        if (length < 1 || length > input.Length)
+            throw new ArgumentException("Invalid length");
+
+        return Enumerable.Range(0, input.Length - length + 1)
+            .Select(i => input.Substring(i, length));
+    }
+}
